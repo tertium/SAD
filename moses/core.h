2c2bd8d755e1af01161c57d87bb2185869eaa95c
Andrei Paskevich <andrei@capet.iut-fbleau.fr>
2007-08-29 23:37:33 +0300

    restore copyrights in Moses

--- moses/core.h
+++ moses/core.h
@@ -1,3 +1,23 @@
+/*
+ *  core.h -- datastructure for inference trees
+ *  Copyright (c) 2001,2004,2007   Andrei Paskevich <atertium@gmail.com>
+ *
+ *  This file is part of SAD/Moses - a simple goal-driven tableau prover.
+ *
+ *  SAD/Moses is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  SAD/Moses is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef CORE_H
 #define CORE_H
 
