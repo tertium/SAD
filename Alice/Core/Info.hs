2b6ac87a33d9cc3d612c9ba03f35bf91fb3024cc
Andrei Paskevich <andrei@capet.iut-fbleau.fr>
2007-09-01 08:24:20 +0300

    turn wfcheck into symbol counter

--- Alice/Core/Info.hs
+++ Alice/Core/Info.hs
@@ -203,25 +203,27 @@ offspring f = let x = children f
 
 -- Well-formedness checking
 
-wfForm fs f | hasInfo f     = wfInfo fs f &&
-                              allF (wfForm fs) (nullInfo f)
-            | otherwise     = allF (wfForm fs) f
+wfForm fs f | hasInfo f     = 1 + wfInfo fs f +
+                                     sumF (wfForm fs) (nullInfo f)
+            | otherwise     = 1 + sumF (wfForm fs) f
 
-wfInfo fs f = all (wfevid (f:fs)) (trInfoI f) &&
-              all (wfevid (f:fs)) (trInfoO f) &&
-              all (wfForm (f:fs)) (trInfoD f)
+wfInfo fs f = sumap (wfevid (f:fs)) (trInfoI f) +
+              sumap (wfevid (f:fs)) (trInfoO f) +
+              sumap (wfForm (f:fs)) (trInfoD f)
   where
-    wfevid fs (Not f) | isTrm f = all (wfargs fs) (trArgs f) && wfInfo fs f
-    wfevid fs f | isTrm f       = all (wfargs fs) (trArgs f) && wfInfo fs f
+    wfevid fs (Not f) | isTrm f = 1 + sumap (wfargs fs) (trArgs f) + wfInfo fs f
+    wfevid fs f | isTrm f       = 1 + sumap (wfargs fs) (trArgs f) + wfInfo fs f
     wfevid fs f                 = wferr "ill-formed info" (f:fs)
 
     wfargs ts t | not (hasInfo t)       = wferr "non-term argument" (t:ts)
                 | not (null $ trInfo t) = wferr "nonempty trInfo" (t:ts)
-                | isTrm t               = all (wfargs ts) (trArgs t)
+                | isTrm t               = 1 + sumap (wfargs ts) (trArgs t)
                 | not (green t)         = wferr "non-green var" (t:ts)
-                | otherwise             = True
+                | otherwise             = 1
 
     wferr es ts = error $ "wfcheck: " ++ es ++ concatMap trout ts
+
+    sumap = (sum .) . map
 
     trout t = " in\n" ++ show t ++ trinf t
     trinf t | hasInfo t = show $ trInfo t
