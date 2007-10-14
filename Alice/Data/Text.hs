fe8cf7bc706bc98580b16bbbc5cb349f11183beb
Andrei Paskevich <andrei@capet.iut-fbleau.fr>
2007-10-14 12:11:48 +0300

    let print instructions when necessary

--- Alice/Data/Text.hs
+++ Alice/Data/Text.hs
@@ -85,10 +85,8 @@ cnRaise cnt cx fs = foldr ((:) . setForm cx) cnt fs
 
 instance Show Text where
   showsPrec p (TB bl) = showsPrec p bl
-{-
-  showsPrec p (TI is) = showsPrec p is . showChar '\n'
-  showsPrec p (TD id) = showsPrec p id . showChar '\n'
--}
+  showsPrec 0 (TI is) = showsPrec 0 is . showChar '\n'
+  showsPrec 0 (TD id) = showsPrec 0 id . showChar '\n'
   showsPrec _ _ = id
 
 instance Show Block where
